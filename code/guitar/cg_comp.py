5899504e78088e7a1c04d65b7d31c4a33457c631
beausol <LyA122nm>
2020-12-16 18:33:18 -0800

    Many major updates and changes. (1) Added data and figures for EJ44 strings. (2) Derived a new equation for the resonant frequency based on hybrid boundary conditions. This reduced the bending stiffness coefficient by a factor of 2. (3) Reran all figures using this new model.

--- code/guitar/cg_comp.py
+++ code/guitar/cg_comp.py
@@ -112,9 +112,16 @@ class GuitarString(object):
     def _comp_modulus(self):
         self._modulus = 1.0e-09 * (self._tension / (np.pi * (self._radius/1000)**2)) * self._kappa
 
-    def _comp_stiffness(self):
+    #def _comp_stiffness(self, bc):
+        #modulus = 1.0e+09 * self._modulus
+        #if bc == 'clamped':
+            #self._stiffness = np.sqrt( np.pi * (self._radius / 1000)**4 * modulus / ( self._tension * (self._scale / 1000)**2 ) )
+        #elif bc == 'pinned':
+            #self._stiffness = (np.pi**2/8) * (self._radius / 1000)**4 * modulus / ( self._tension * (self._scale / 1000)**2 )
+
+    def _comp_stiffness(self, bc):
         modulus = 1.0e+09 * self._modulus
-        self._stiffness = np.sqrt( np.pi * (self._radius / 1000)**4 * modulus / ( self._tension * (self._scale / 1000)**2 ) )
+        self._stiffness = np.sqrt( np.pi * (self._radius / 1000)**4 * modulus / ( 4 * self._tension * (self._scale / 1000)**2 ) )
 
     def set_scale(self, scale_length, units=None):
         if units == 'IPS':
@@ -124,11 +131,11 @@ class GuitarString(object):
         self._scale = scale_length
         self._tension = self._comp_tension()
         
-    def set_r(self, r):
+    def set_r(self, r, bc):
         self._r = r
         self._comp_kappa()
         self._comp_modulus()
-        self._comp_stiffness()
+        self._comp_stiffness(bc)
     
     def get_tension(self):
         return self._tension
@@ -182,15 +189,35 @@ class GuitarStrings(object):
             retstr += string.__str__() + "\n"
         return retstr
 
-    def comp_r(self, delta_nu:list, g, b, c, ds, dn, x0):
+    #def comp_r(self, delta_nu:list, g, b, c, ds, dn, x0, bc):
+        #q = ( g / (2 * x0**2) ) * ( (b + c)**2 + b**2/(g - 1) - c**2/g )
+        #l0 = x0 + ds + dn
+
+        #kappa = np.zeros_like(l0)
+        #string_num = np.arange(0, len(self._strings))
+        #if bc == 'clamped':
+            #aa = q / 2
+            #for string, num in zip(self._strings, string_num):
+                #bb = string._radius / l0[num]
+                #cc = -(np.log(2)/1200.0) * delta_nu[num] - (g - 1) * (ds[num] - dn[num]) / x0
+                #kappa[num] = ( ( -bb + np.sqrt(bb**2 - 4 * aa * cc) ) / (2 * aa) )**2
+        #else:   # bc == 'pinned'
+            #b0 = (g**2 - 1) * (np.pi**2/8) * (string._radius / l0[num])**2
+            #denom = b0 + q / 2
+            #numer = (np.log(2)/1200.0) * delta_nu[num] + ((g - 1) * ds[num] - dn[num]) / x0
+            #kappa[num] = numer / denom
+
+        #return (600.0 / np.log(2)) * (kappa + 1)
+
+    def comp_r(self, delta_nu:list, g, b, c, ds, dn, x0, bc):
         q = ( g / (2 * x0**2) ) * ( (b + c)**2 + b**2/(g - 1) - c**2/g )
-        aa = q / 2
         l0 = x0 + ds + dn
 
         kappa = np.zeros_like(l0)
         string_num = np.arange(0, len(self._strings))
+        aa = q / 2
         for string, num in zip(self._strings, string_num):
-            bb = string._radius / l0[num]
+            bb = string._radius / (2 * l0[num])
             cc = -(np.log(2)/1200.0) * delta_nu[num] - (g - 1) * (ds[num] - dn[num]) / x0
             kappa[num] = ( ( -bb + np.sqrt(bb**2 - 4 * aa * cc) ) / (2 * aa) )**2
 
@@ -201,9 +228,9 @@ class GuitarStrings(object):
         for string in self._strings:
             string.set_scale(scale_length)
 
-    def set_r(self, r):
+    def set_r(self, r, bc):
         for string, r_string in zip(self._strings, r):
-            string.set_r(r_string)
+            string.set_r(r_string, bc)
 
     def get_count(self):
         return len(self._strings)
@@ -240,7 +267,7 @@ class GuitarStrings(object):
 
 
 class Guitar(object):
-    def __init__(self, name, x0, dn, ds, b, c, string_count, strings):
+    def __init__(self, name, x0, dn, ds, b, c, string_count, strings, d=0, bc='clamped'):
         self._name = name
         self._x0 = x0
         if len(dn) == 1:
@@ -253,6 +280,8 @@ class Guitar(object):
             self._ds = np.array(ds)
         self._b = b
         self._c = c
+        self._d = d
+        self._bc = bc
         
         assert string_count == strings.get_count(), "Guitar '{}'".format(self._name) + " requires {} strings, but {} were provided.".format(string_count, strings.get_count())
         self.string_list = np.arange(1, string_count + 1)
@@ -287,18 +316,15 @@ class Guitar(object):
             retstr += setback_str[0:-2] + ']\n'
         retstr += 'b: ' + '{:.1f} mm\n'.format(self._b)
         retstr += 'c: ' + '{:.1f} mm\n'.format(self._c)
+        retstr += 'd: ' + '{:.1f} mm\n'.format(self._d)
         retstr += self._strings.__str__() + "\n"
         return retstr
     
+    def get_bc(self):
+        return self._bc
+
     def gamma(self, n):
         return 2.0**(n/12.0)
-
-    #def l(self, n):
-        #if n != 0:
-            #length = np.sqrt( (self._x0/self.gamma(n) + self._ds)**2 + (self._b + self._c)**2 )
-        #else:
-            #length = np.sqrt( (self._x0 + self._ds + self._dn)**2 + self._c**2 )
-        #return length
 
     def l0(self):
         length = np.sqrt( (self._x0 + self._ds + self._dn)**2 + self._c**2 )
@@ -311,7 +337,7 @@ class Guitar(object):
 
     def lp(self, fret_list):
         dn, n = np.meshgrid(self._dn, fret_list, sparse=False, indexing='ij')
-        length = np.sqrt( (dn + self._x0 - self._x0/self.gamma(n))**2 + self._b**2 )
+        length = self._d + np.sqrt( (dn + self._x0 - self._x0/self.gamma(n) - self._d)**2 + self._b**2 )
         return length
 
     def lmc(self, fret_list):
@@ -339,5 +365,5 @@ class Guitar(object):
     
     def comp_r(self, delta_nu:list, fret:int):
         g = self.gamma(fret)
-        r = self._strings.comp_r(delta_nu, g, self._b, self._c, self._ds, self._dn, self._x0)
+        r = self._strings.comp_r(delta_nu, g, self._b, self._c, self._ds, self._dn, self._x0, bc=self._bc)
         return r
