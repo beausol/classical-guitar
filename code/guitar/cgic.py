259013fad76157f8e616fed47efde5f3e3b1aa0d
Raymond Beausoleil <ray.beausoleil@hpe.com>
2024-03-02 18:37:13 -0800

    Updates to cgic.py for new compensation algorithms
    Improved approximation approach; added Guitar.compin for final minimization of setbacks; took all length functions private

--- code/guitar/cgic.py
+++ code/guitar/cgic.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import display
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, minimize
 
 # Define a reasonable set of matplotlib parameters compatible
 #  with Jupyter notebooks
@@ -531,8 +531,6 @@ class Guitar(object):
             rgx = rgx
         )
         
-
-        
     def __str__(self):
         '''Return a string displaying the attributes of a Guitar object.
 
@@ -610,11 +608,8 @@ class Guitar(object):
     def _tile_frets(self, x, fret_list):
         return np.tile(x, (fret_list.size, 1)).T
     
-    def _rms(self):
-        fret_list = np.arange(1, 13)
-        dnu = self.freq_shifts(fret_list)
-        rms = np.sqrt(np.mean(dnu[:,1:]**2, axis=1))
-        
+    def _rms(self, dnu):
+        rms = np.sqrt(np.mean(dnu**2))
         return rms
 
     def set_vars(self, **kwargs):
@@ -660,11 +655,15 @@ class Guitar(object):
         else:
             self._rgx = self._setarr(rgx)
 
-    def gamma(self, n):
+    def _gamma(self, n):
         return 2.0**(n/12.0)
 
     def l0(self):
         length = np.sqrt( (self._x0 + self._ds + self._dn)**2 + self._c**2 )
+        return length
+
+    def _l0(self, ds, dn, x0, c):
+        length = np.sqrt( (x0 + ds + dn)**2 + c**2 )
         return length
 
     def l(self, fret_list):
@@ -674,7 +673,18 @@ class Guitar(object):
         c = self._tile_frets(self._c, fret_list)
         n = self._tile_strings(fret_list)
 #        length = np.sqrt( (self._x0/self.gamma(n) + ds)**2 + (self._b + self._c)**2 )
-        length = np.sqrt( (self._x0/self.gamma(n) + ds)**2 + (b + c)**2 )
+        length = np.sqrt( (self._x0/self._gamma(n) + ds)**2 + (b + c)**2 )
+        return length
+
+    def _l(self, ds, x0, b, c, n):
+        length = np.sqrt( (x0/self._gamma(n) + ds)**2 + (b + c)**2 )
+        return length
+
+    def _lp(self, ds, dn, x0, b, c, d, n):
+        xn = x0 / self._gamma(n)
+        ln = self._l(ds, x0, b, c, n)
+        length = np.sqrt( (x0 - xn + dn - d)**2 + (b + (b + c) * d / (xn + ds))**2 )
+        length += ( ln / (xn + ds)  ) * d
         return length
 
     def lp(self, fret_list):
@@ -685,7 +695,7 @@ class Guitar(object):
         c = self._tile_frets(self._c, fret_list)
         d = self._d
         n = self._tile_strings(fret_list)
-        xn = x0 / self.gamma(n)
+        xn = x0 / self._gamma(n)
         #length = np.sqrt( (dn + self._x0 - self._x0/self.gamma(n))**2 + self._bn(n)**2 )
         length = np.sqrt( (x0 - xn + dn - d)**2 + (b + (b + c) * d / (xn + ds))**2 )
         length += ( self.l(fret_list) / (xn + ds)  ) * d
@@ -695,7 +705,7 @@ class Guitar(object):
         dn, n = np.meshgrid(self._dn, fret_list, sparse=False, indexing='ij')
         ds, n = np.meshgrid(self._ds, fret_list, sparse=False, indexing='ij')
         x0 = self._x0
-        xn = x0 / self.gamma(n)
+        xn = x0 / self._gamma(n)
         b = self._b
         c = self._c
         d = self._d
@@ -708,16 +718,60 @@ class Guitar(object):
         length = self.l(fret_list) + self.lp(fret_list)
         return length
     
+    def _lmc(self, ds, dn, x0, b, c, d, n):
+        length = self._l(ds, x0, b, c, n) + self._lp(ds, dn, x0, b, c, d, n)
+        return length
+    
     def qn(self, fret_list):
 #        l0 = np.tile(self.l0().reshape(-1, 1), (1, fret_list.size))
         l0 = self._tile_frets(self.l0(), fret_list)
         return (self.lmc(fret_list) - l0) / l0
     
-    # def qnx(self, fret_list):
-    #     retval = ( (self.gamma(fret_list) - 1)/(2 * self._x0**2) ) * ( (self.gamma(fret_list)/(self.gamma(fret_list) - 1)) * self._bn(fret_list) + self._c )**2
-    #     return retval
+    def _q(self, ds, dn, x0, b, c, d, n):
+#        l0 = np.tile(self.l0().reshape(-1, 1), (1, fret_list.size))
+        l0 = self._l0(ds, dn, x0, c)
+        return (self._lmc(ds, dn, x0, b, c, d, n) - l0) / l0
+    
+    def _rle(self, ds, dn, x0, b, c, n):
+        return 1200 * np.log2( self._l0(ds, dn, x0, c) / (self._gamma(n) * self._l(ds, x0, b, c, n)) )
+    
+    def _mde(self, ds, dn, x0, b, c, d, n):
+        return 600 * np.log2( 1 + self._q(ds, dn, x0, b, c, d, n) )
+    
+    def _tse(self, ds, dn, x0, b, c, d, kappa, n):
+        return 600 * np.log2( 1 + kappa * self._q(ds, dn, x0, b, c, d, n) )
+
+    def _bse(self, b_0, n):
+        return 1200 * np.log2( (1 + self._gamma(n) * b_0 + (1.0 + 0.5 * np.pi**2) * (self._gamma(n) * b_0)**2)
+                             / (1 + b_0 + (1.0 + 0.5 * np.pi**2) * b_0**2) )
+        
+    def _tfe(self, ds, dn, x0, b, c, d, kappa, b_0, n):
+        rle = self._rle(ds, dn, x0, b, c, n)
+        mde = self._mde(ds, dn, x0, b, c, d, n)
+        tse = self._tse(ds, dn, x0, b, c, d, kappa, n)
+        bse = self._bse(b_0, n)
+        
+        return rle + mde + tse + bse
+
     
     def freq_shifts(self, fret_list):
+        x0 = self._x0
+        ds = self._tile_frets(self._ds, fret_list)
+        dn = self._tile_frets(self._dn, fret_list)
+        b = self._tile_frets(self._b, fret_list)
+        c = self._tile_frets(self._c, fret_list)
+        d = self._d
+        kappa = self._tile_frets(self._strings.get_kappa(), fret_list)
+        b_0 = self._tile_frets(self._strings.get_stiffness() * self._rgx , fret_list)
+        n = self._tile_strings(fret_list)
+        
+        shifts = self._tfe(ds, dn, x0, b, c, d, kappa, b_0, n)
+        
+        open_strings = np.array([np.zeros(self._string_list[-1])]).T
+        
+        return np.hstack((open_strings, shifts))
+    
+    def freq_shifts_old(self, fret_list):
         l_0 = self._tile_frets(self.l0(), fret_list)
         kappa = self._tile_frets(self._strings.get_kappa(), fret_list)
         b_0 = self._tile_frets(self._strings.get_stiffness() * self._rgx , fret_list)
@@ -733,10 +787,10 @@ class Guitar(object):
         # #bse = 1200 * np.log2( (1 + self.gamma(n_2d) * b_0) / (1 + b_0) )
         # bse = 1200 * np.log2( (1 + self.gamma(n_2d) * b_0 + (1.0 + 0.5 * np.pi**2) * (self.gamma(n_2d) * b_0)**2)
         #                       / (1 + b_0 + (1.0 + 0.5 * np.pi**2) * b_0**2) )
-        rle = 1200 * np.log2( l_0 / (self.gamma(n) * self.l(fret_list)) )
+        rle = 1200 * np.log2( l_0 / (self._gamma(n) * self.l(fret_list)) )
         mde =  600 * np.log2( 1 + self.qn(fret_list) )
         tse =  600 * np.log2( 1 + kappa * self.qn(fret_list) )
-        bse = 1200 * np.log2( (1 + self.gamma(n) * b_0 + (1.0 + 0.5 * np.pi**2) * (self.gamma(n) * b_0)**2)
+        bse = 1200 * np.log2( (1 + self._gamma(n) * b_0 + (1.0 + 0.5 * np.pi**2) * (self._gamma(n) * b_0)**2)
                             / (1 + b_0 + (1.0 + 0.5 * np.pi**2) * b_0**2) )
         
         shifts = rle + mde + tse + bse
@@ -751,12 +805,12 @@ class Guitar(object):
     #     r = self._strings.estimate_r(delta_nu, g, q, self._ds, self._dn, self._x0)
     #     return r
     
-    def compensate_old(self, max_fret:int):
+    def compensate_older(self, max_fret:int):
         self._ds = np.array([0.0] * self._strings.get_count())
         self._dn = np.array([0.0] * self._strings.get_count())
         
         fret_list = np.arange(1, max_fret + 1)
-        g_n = self.gamma(fret_list)
+        g_n = self._gamma(fret_list)
         q_n = self.qn(fret_list)[0]
 
         ds, dn = self._strings.compensate(g_n, q_n, self._rgx)
@@ -767,7 +821,89 @@ class Guitar(object):
 
     def compensate(self, max_fret:int):
         def sigma_k(fret_list, k):
-            return np.sum((self.gamma(fret_list) - 1)**k)
+            return np.sum((self._gamma(fret_list) - 1)**k)
+
+        fret_list = np.arange(1, max_fret + 1)
+
+        x0 = self._x0
+        b = self._tile_frets(self._b, fret_list)
+        c = self._tile_frets(self._c, fret_list)
+        d = self._d
+        kappa = self._tile_frets(self._strings.get_kappa(), fret_list)
+        b_0 = self._tile_frets(self._strings.get_stiffness() * self._rgx , fret_list)
+        n = self._tile_strings(fret_list)
+        ds = np.zeros(n.shape)
+        dn = np.zeros(n.shape)
+        
+        mde = self._mde(ds, dn, x0, b, c, d, n)
+        tse = self._tse(ds, dn, x0, b, c, d, kappa, n)
+        bse = self._bse(b_0, n)
+        
+        z_n = (np.log(2.0) / 1200.0) * ( mde + tse + bse )
+        
+        sigma_0 = max_fret
+        sigma_1 = sigma_k(fret_list, 1)
+        sigma_2 = sigma_k(fret_list, 2)
+        
+        zbar_0 = np.sum(z_n, axis=1)
+        zbar_1 = np.sum((self._gamma(fret_list) - 1) * z_n, axis=1)
+        
+        det = sigma_0 * sigma_2 - sigma_1**2
+        ds =  ( sigma_0 * zbar_1 - sigma_1 * zbar_0 ) * self._x0 / det
+        dn = -( sigma_2 * zbar_0 - sigma_1 * zbar_1 ) * self._x0 / det
+
+        self._ds = ds
+        self._dn = dn
+        
+        return ds, dn
+
+    def compmin(self, max_fret:int, approx:bool=False):
+        def minfun(dsdn, *args):
+            ds = dsdn[0]
+            dn = dsdn[1]
+            x0 = args[0]
+            b = args[1]
+            c = args[2]
+            d = args[3]
+            kappa = args[4]
+            b_0 = args[5]
+            n = args[6]
+            
+            tfe = self._tfe(ds, dn, x0, b, c, d, kappa, b_0, n)
+            
+            return np.sqrt(np.mean(tfe**2))
+        
+        fret_list = np.arange(1, max_fret + 1)
+
+        if approx:
+            ds_0, dn_0 = self.approximate()
+        else:
+            ds_0, dn_0 = self.compensate(max_fret)
+        x0 = self._x0
+        b = self._b
+        c = self._c
+        d = self._d
+        kappa = self._strings.get_kappa()
+        b_0 = self._strings.get_stiffness() * self._rgx
+
+        ds = np.zeros_like(ds_0)
+        dn = np.zeros_like(dn_0)
+        idx_list = np.arange(0, self._strings.get_count())
+        for idx in idx_list:
+            dsdn0 = np.array([ds_0[idx], dn_0[idx]])
+            args = (x0, b[idx], c[idx], d, kappa[idx], b_0[idx], fret_list)
+            res = minimize(minfun, dsdn0, args)
+            ds[idx] = res.x[0]
+            dn[idx] = res.x[1]
+            
+        self._ds = ds
+        self._dn = dn
+    
+        return ds, dn
+
+    def compensate_old(self, max_fret:int):
+        def sigma_k(fret_list, k):
+            return np.sum((self._gamma(fret_list) - 1)**k)
 
         fret_list = np.arange(1, max_fret + 1)
 
@@ -781,7 +917,7 @@ class Guitar(object):
 
         mde =  600 * np.log2( 1 + self.qn(fret_list) )
         tse =  600 * np.log2( 1 + kappa * self.qn(fret_list) )
-        bse = 1200 * np.log2( (1 + self.gamma(n) * b_0 + (1.0 + 0.5 * np.pi**2) * (self.gamma(n) * b_0)**2)
+        bse = 1200 * np.log2( (1 + self._gamma(n) * b_0 + (1.0 + 0.5 * np.pi**2) * (self._gamma(n) * b_0)**2)
                             / (1 + b_0 + (1.0 + 0.5 * np.pi**2) * b_0**2) )
         
         z_n = mde + tse + bse
@@ -791,7 +927,7 @@ class Guitar(object):
         sigma_2 = sigma_k(fret_list, 2)
         
         zbar_0 = np.sum(z_n, axis=1)
-        zbar_1 = np.sum((self.gamma(fret_list) - 1) * z_n, axis=1)
+        zbar_1 = np.sum((self._gamma(fret_list) - 1) * z_n, axis=1)
         
         det = sigma_0 * sigma_2 - sigma_1**2
         ds =  ( sigma_0 * zbar_1 - sigma_1 * zbar_0 ) * self._x0 / det
@@ -802,7 +938,7 @@ class Guitar(object):
         
         return ds, dn
 
-    def approximate(self, max_fret:int):
+    def approximate(self):
         x_0 = self._x0
         b = self._b
         c = self._c
@@ -810,9 +946,9 @@ class Guitar(object):
         kappa = self._strings.get_kappa()
         b_0 = self._strings.get_stiffness() * self._rgx
 
-        fret_d = kappa * (2*b + c)**2 * d / x_0**2
-        ds = b_0 * x_0 + kappa * ( (b + c)**2 - 4 * b**2 ) / (4 * x_0) - fret_d
-        dn = -(kappa/2) * (7 * b**2 + 2*b*c)/(2 * x_0) - fret_d
+        fret_d = kappa * 2 * (2*b + c)**2 * d / x_0**2
+        ds = b_0 * x_0 + kappa * ( (b + c)**2 - 7 * b**2 ) / (4 * x_0) - fret_d
+        dn = -kappa * b * (5 * b + c)/(2 * x_0) - fret_d
 
         self._ds = ds
         self._dn = dn
@@ -830,7 +966,7 @@ class Guitar(object):
     #     else:
     #         self._dn = np.array(dn)
 
-    def plot_shifts_old(self, max_fret=12, savepath=None, filename=None):
+    def plot_shifts_older(self, max_fret=12, savepath=None, filename=None):
         fret_list = np.arange(0, max_fret + 1)
         shifts = self.freq_shifts(fret_list[1:])
         rms = np.sqrt(np.mean(shifts**2))
@@ -880,6 +1016,56 @@ class Guitar(object):
         plt.show()
 
     def plot_shifts(self, max_fret=12, show=True, harm=[], savepath=None, filename=None):
+        fret_list = np.arange(0, max_fret + 1)
+        shifts = self.freq_shifts(fret_list[1:])
+        if harm:
+            zero_strings = harm[0]
+            zero_frets = harm[1]
+            for s, n in zip(zero_strings, zero_frets):
+                shifts[s-1] -= shifts[s-1][n]
+        rms = np.sqrt(np.mean(shifts**2))
+        names = self._strings.get_string_names()
+
+        plt.figure(figsize=(8.0,6.0))
+        for string in self._string_list:
+            plt.plot(fret_list, shifts[string-1], label='{}'.format(names[string-1]))
+        plt.xlabel('FRET', fontdict=font)
+        plt.ylabel('SHIFT (cents)', fontdict=font)
+        plt.tick_params(axis='x', labelsize=labelsize)
+        plt.tick_params(axis='y', labelsize=labelsize)
+        plt.xlim(fret_list[0],fret_list[-1])
+        plt.ylim(-4,10)
+        plt.legend(loc='upper right', fontsize=labelsize)
+        
+        if np.all(np.abs(self._ds - self._ds[0]) < 1.0e-06):
+            ds = self._ds[0]
+            template_ds = '$\Delta S = {}~\mathrm{{mm}}$'
+        else:
+            ds = np.round(np.mean(self._ds), 2)
+            template_ds = '$\Delta S = {}~\mathrm{{mm~(mean)}}$'
+        if np.all(np.abs(self._dn - self._dn[0]) < 1.0e-06):
+            dn = self._dn[0]
+            template_dn = '$\Delta N = {}~\mathrm{{mm}}$'
+        else:
+            dn = np.round(np.mean(self._dn), 2)
+            template_dn = '$\Delta N = {}~\mathrm{{mm~(mean)}}$'
+        template = '{}\n' + template_ds + '\n' + template_dn + '\n' + 'Shift~(rms):~{}~cents'
+        plt.text(0.35, 5.3, template.format(self._name, ds, dn, np.round(rms, 2)),
+                 fontdict=font, bbox=bbox)
+        plt.grid(True)
+        
+        filepath = file_path(savepath, filename)
+        if filepath is None:
+            pass
+        else:
+            plt.savefig(filepath, bbox_inches='tight')
+            print("Saved {0}\n".format(filepath))
+        if show:
+            plt.show()
+        else:
+            plt.close()
+
+    def plot_shifts_old(self, max_fret=12, show=True, harm=[], savepath=None, filename=None):
         fret_list = np.arange(0, max_fret + 1)
         shifts = self.freq_shifts(fret_list[1:])
         if harm:
@@ -998,8 +1184,11 @@ class Guitar(object):
     #         plt.savefig(filepath, bbox_inches='tight')
     #     plt.show()
     
-    def save_setbacks_table(self, show=True, savepath=None, filename=None):
-        rms = self._rms()
+    def save_setbacks_table(self, max_fret:int=12, show:bool=True, savepath=None, filename=None):
+        fret_list = np.arange(1, max_fret)
+        dnu = self.freq_shifts(fret_list)
+        rms = np.sqrt(np.mean(dnu[:,1:]**2, axis=1))
+
         df = pd.DataFrame({'String': self._strings.get_string_names(),
                            '$\Delta S$ (mm)': self._ds.tolist(),
                            '$\Delta N$ (mm)': self._dn.tolist(),
